8bf3d86364c2ed8289eb58520fb0bf292ee29760
Andy Guo <>
2021-04-30 17:47:41 +0000

    Review 6 - complete

--- src/gr11review/part1/Review6.java
+++ src/gr11review/part1/Review6.java
@@ -1,6 +1,7 @@
 package gr11review.part1;
 
 import java.io.*;
+import java.text.*;
 
 /*
 Compile command:
@@ -25,3 +26,37 @@ java -jar lib/junit-platform-console-standalone-1.7.0-M1.jar --class-path bin -c
 * 
 * @author: Andy Guo
 */
+
+public class Review6 {
+  public static void main(String[] args) throws IOException {
+    // BufferedReader
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    // Declaring variables + initializing
+    double subtotal = 0;
+    double tax;
+    double total;
+    double itemPrice = -1d;
+    int numberOfItems = 0;
+
+    // Decimal format
+    NumberFormat numberFormat = new DecimalFormat("#,##0.00");
+
+    // Loop
+    while (itemPrice != 0) {
+      // Takes an item and adds it to the subtotal
+      System.out.print("Enter the price for an item: ");
+      itemPrice = Double.parseDouble(br.readLine());
+      subtotal += itemPrice;
+    }
+
+    // Calculates tax and total
+    tax = subtotal * 0.13;
+    total = subtotal + tax;
+
+    //Output
+    System.out.println("Subtotal: $" + numberFormat.format(subtotal));
+    System.out.println("Tax: $" + numberFormat.format(tax));
+    System.out.println("Total: $" + numberFormat.format(total));
+  }
+}
